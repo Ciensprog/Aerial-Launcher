8a17e8342bcbf0e00594e31954f23f107da1122f
YoJellyfish <165499511+YoJellyfish@users.noreply.github.com>
2024-04-14 15:49:51 -0600

    feat: apply ordering

--- src/kernel/startup/accounts.ts
+++ src/kernel/startup/accounts.ts
@@ -12,11 +12,15 @@ import { DataDirectory } from './data-directory'
 export class AccountsManager {
   static async load(currentWindow: BrowserWindow) {
     const result = await DataDirectory.getAccountsFile()
-    const accounts: AccountDataList = result.accounts.map((account) => ({
-      ...account,
-      provider: undefined,
-      token: undefined,
-    }))
+    const accounts: AccountDataList = result.accounts
+      .map((account) => ({
+        ...account,
+        provider: undefined,
+        token: undefined,
+      }))
+      .toSorted((itemA, itemB) =>
+        itemA.displayName.localeCompare(itemB.displayName)
+      )
 
     const accountList = accounts.reduce((accumulator, current) => {
       accumulator[current.accountId] = current
