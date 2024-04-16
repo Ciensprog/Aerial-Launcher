771448ac931c76cd69a5c7f338609bb8670b1e4c
YoJellyfish <165499511+YoJellyfish@users.noreply.github.com>
2024-04-15 20:04:38 -0600

    refactor: extract common functionality

--- src/routes/accounts/add/(exchange-code)/-hooks.ts
+++ src/routes/accounts/add/(exchange-code)/-hooks.ts
@@ -1,14 +1,8 @@
 import { zodResolver } from '@hookform/resolvers/zod'
-import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
-import { useShallow } from 'zustand/react/shallow'
 
-import { requestData } from '../../../../bootstrap/components/load-accounts'
-
-import { useAccountListStore } from '../../../../state/accounts/list'
-
-import { toast } from '../../../../lib/notifications'
+import { useBaseSetupForm } from '../-hooks'
 
 const formSchema = z.object({
   code: z.string().length(32, {
@@ -17,13 +11,6 @@ const formSchema = z.object({
 })
 
 export function useSetupForm() {
-  const { addOrUpdate, changeSelected, register } = useAccountListStore(
-    useShallow((state) => ({
-      addOrUpdate: state.addOrUpdate,
-      changeSelected: state.changeSelected,
-      register: state.register,
-    }))
-  )
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -31,47 +18,9 @@ export function useSetupForm() {
     },
   })
 
-  useEffect(() => {
-    const listener = window.electronAPI.responseAuthWithExchange(
-      async ({ data, error }) => {
-        if (data) {
-          const accountsToArray = Object.values(data.accounts)
-
-          register({
-            [data.currentAccount.accountId]: data.currentAccount,
-          })
-
-          if (accountsToArray.length <= 1) {
-            changeSelected(accountsToArray[0].accountId)
-          }
-
-          requestData(data.currentAccount)
-            .then((response) => {
-              addOrUpdate(response.accountId, {
-                ...data.currentAccount,
-                provider: response.provider ?? null,
-                token: response.token ?? null,
-              })
-            })
-            .catch(() => {
-              addOrUpdate(data.currentAccount.accountId, {
-                ...data.currentAccount,
-                provider: null,
-                token: null,
-              })
-            })
-
-          toast(`New account added: ${data.currentAccount.displayName}`)
-        } else if (error) {
-          toast(error ?? 'Unknown error :c')
-        }
-      }
-    )
-
-    return () => {
-      listener.removeListener()
-    }
-  }, [])
+  useBaseSetupForm({
+    fetcher: window.electronAPI.responseAuthWithExchange,
+  })
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
     window.electronAPI.createAuthWithExchange(values.code)
--- src/routes/accounts/add/-hooks.ts
+++ src/routes/accounts/add/-hooks.ts
@@ -1,6 +1,16 @@
 import type { MouseEventHandler } from 'react'
+import type { AuthCallbackFunction } from '../../../types/preload'
+
+import { useEffect } from 'react'
+import { useShallow } from 'zustand/react/shallow'
 
 import { epicGamesAuthorizationCodeURL } from '../../../config/fortnite/links'
+
+import { requestData } from '../../../bootstrap/components/load-accounts'
+
+import { useAccountListStore } from '../../../state/accounts/list'
+
+import { toast } from '../../../lib/notifications'
 
 export function useHandlers() {
   const goToAuthorizationCodeURL: MouseEventHandler<HTMLAnchorElement> = (
@@ -13,4 +23,60 @@ export function useHandlers() {
   return {
     goToAuthorizationCodeURL,
   }
+}
+
+export function useBaseSetupForm({
+  fetcher,
+}: {
+  fetcher: (callback: AuthCallbackFunction) => {
+    removeListener: () => Electron.IpcRenderer
+  }
+}) {
+  const { addOrUpdate, changeSelected, register } = useAccountListStore(
+    useShallow((state) => ({
+      addOrUpdate: state.addOrUpdate,
+      changeSelected: state.changeSelected,
+      register: state.register,
+    }))
+  )
+
+  useEffect(() => {
+    const listener = fetcher(async ({ data, error }) => {
+      if (data) {
+        const accountsToArray = Object.values(data.accounts)
+
+        register({
+          [data.currentAccount.accountId]: data.currentAccount,
+        })
+
+        if (accountsToArray.length <= 1) {
+          changeSelected(accountsToArray[0].accountId)
+        }
+
+        requestData(data.currentAccount)
+          .then((response) => {
+            addOrUpdate(response.accountId, {
+              ...data.currentAccount,
+              provider: response.provider ?? null,
+              token: response.token ?? null,
+            })
+          })
+          .catch(() => {
+            addOrUpdate(data.currentAccount.accountId, {
+              ...data.currentAccount,
+              provider: null,
+              token: null,
+            })
+          })
+
+        toast(`New account added: ${data.currentAccount.displayName}`)
+      } else if (error) {
+        toast(error ?? 'Unknown error :c')
+      }
+    })
+
+    return () => {
+      listener.removeListener()
+    }
+  }, [])
 }
