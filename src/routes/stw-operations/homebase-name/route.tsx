b247b9cb5b23c5f4f35e3dd3c9030ad87feaedb7
Ciensprog <jorg.leonrd@gmail.com>
2024-07-05 00:15:38 -0600

    feat: add new page | Homebase Name

---
+++ src/routes/stw-operations/homebase-name/route.tsx
@@ -0,0 +1,49 @@
+import { Link, createRoute } from '@tanstack/react-router'
+
+import { Route as RootRoute } from '../../__root'
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '../../../components/ui/breadcrumb'
+
+// import { useData } from './-hooks'
+
+export const Route = createRoute({
+  getParentRoute: () => RootRoute,
+  path: '/stw-operations/homebase-name',
+  component: Content,
+})
+
+function Content() {
+  return (
+    <>
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link to="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>STW Operations</BreadcrumbPage>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Homebase Name</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      <div className="flex flex-grow">
+        <div className="flex items-center justify-center w-full">
+          Coming Soon!
+        </div>
+      </div>
+    </>
+  )
+}
