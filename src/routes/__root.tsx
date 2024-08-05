477dbcfe6b3e0861058116e52e502a3c85bcaca3
Ciensprog <jorg.leonrd@gmail.com>
2024-08-05 16:39:57 -0600

    feat: scroll to top when pathname is changing

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,13 +1,40 @@
-import { createRootRoute, Outlet } from '@tanstack/react-router'
+import {
+  createRootRoute,
+  Outlet,
+  useLocation,
+} from '@tanstack/react-router'
+import { useEffect, useRef } from 'react'
 
 import { MainLayout } from '../layouts/main'
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <MainLayout>
-        <Outlet />
-      </MainLayout>
-    </>
-  ),
+  component: () => {
+    useScrollToTop()
+
+    return (
+      <>
+        <MainLayout>
+          <Outlet />
+        </MainLayout>
+      </>
+    )
+  },
 })
+
+function useScrollToTop() {
+  const $main = useRef<HTMLElement | null>(null)
+  const pathname = useLocation({
+    select: (location) => location.pathname,
+  })
+
+  useEffect(() => {
+    if (!$main.current) {
+      $main.current = document.querySelector('main')
+    }
+
+    $main.current?.scroll({
+      behavior: 'instant',
+      top: 0,
+    })
+  }, [pathname])
+}
