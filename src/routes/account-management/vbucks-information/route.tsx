2ba1c96a6c6dd50a2ced1aabeea5c10db9dc343c
Ciensprog <jorg.leonrd@gmail.com>
2024-10-08 15:36:40 -0600

    feat: add new page | V-Bucks Information

---
+++ src/routes/account-management/vbucks-information/route.tsx
@@ -0,0 +1,96 @@
+import { createRoute } from '@tanstack/react-router'
+import { UpdateIcon } from '@radix-ui/react-icons'
+
+import { Route as RootRoute } from '../../__root'
+
+import { HomeBreadcrumb } from '../../../components/navigations/breadcrumb/home'
+import { AccountSelectors } from '../../../components/selectors/accounts'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from '../../../components/ui/breadcrumb'
+import { Button } from '../../../components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from '../../../components/ui/card'
+
+// import {  } from './-hooks'
+
+// import { cn, parseCustomDisplayName } from '../../../lib/utils'
+
+export const Route = createRoute({
+  getParentRoute: () => RootRoute,
+  path: '/account-management/vbucks-information',
+  component: () => {
+    return (
+      <>
+        <Breadcrumb>
+          <BreadcrumbList>
+            <HomeBreadcrumb />
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>Account Management</BreadcrumbPage>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <BreadcrumbPage>V-Bucks Information</BreadcrumbPage>
+            </BreadcrumbItem>
+          </BreadcrumbList>
+        </Breadcrumb>
+        <Content />
+      </>
+    )
+  },
+})
+
+function Content() {
+  return (
+    <div className="flex flex-grow">
+      <div className="flex items-center justify-center w-full">
+        <div className="max-w-lg space-y-4 w-full">
+          <Card className="w-full">
+            <CardHeader className="border-b">
+              <CardDescription>
+                V-Bucks information on each selected accounts.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="grid gap-4 pt-6">
+              <AccountSelectors
+                accounts={{
+                  options: [],
+                  value: [],
+                }}
+                tags={{
+                  options: [],
+                  value: [],
+                }}
+                // onUpdateAccounts={}
+                // onUpdateTags={}
+              />
+            </CardContent>
+            <CardFooter className="space-x-6">
+              <Button
+                className="w-full"
+                // onClick={}
+                // disabled={isDisabledForm}
+              >
+                {false ? (
+                  <UpdateIcon className="animate-spin" />
+                ) : (
+                  'Show Information'
+                )}
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </div>
+    </div>
+  )
+}
